402890ee3d50e16581385c47d8bf5d52f9aa5007
pauldardeau <pauldardeau@me.com>
2017-04-18 21:08:59 -0500

    implement ServerDateTime handler

--- JavaMisere/src/com/swampbits/misere/handler/ServerDateTimeHandler.java
+++ JavaMisere/src/com/swampbits/misere/handler/ServerDateTimeHandler.java
@@ -5,6 +5,10 @@
  */
 package com.swampbits.misere.handler;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import com.swampbits.misere.HttpRequest;
 import com.swampbits.misere.HttpResponse;
 
@@ -13,11 +17,19 @@ import com.swampbits.misere.HttpResponse;
  * @author paul
  */
 public class ServerDateTimeHandler extends AbstractHandler {
+   
+   private final DateFormat df;
+   
+   public ServerDateTimeHandler() {
+      df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+   }
 
    @Override
    public void serviceRequest(HttpRequest request,
                               HttpResponse response) {
-      
+      StringBuilder body = new StringBuilder("<html><body>");
+      body.append(df.format(new Date()));
+      body.append("</body></html>");
+      response.setBody(body.toString());
    }
-
 }
