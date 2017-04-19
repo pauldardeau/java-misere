bf542f943f0569adaacf73be26fb274afaa34958
pauldardeau <pauldardeau@me.com>
2017-04-19 09:30:00 -0500

    implement server stats handler as count of log levels logged

--- JavaMisere/src/com/swampbits/misere/handler/ServerStatsHandler.java
+++ JavaMisere/src/com/swampbits/misere/handler/ServerStatsHandler.java
@@ -5,6 +5,10 @@
  */
 package com.swampbits.misere.handler;
 
+import java.util.Map;
+
+import com.swampbits.chaudiere.Logger;
+import com.swampbits.chaudiere.StdLogger;
 import com.swampbits.misere.HttpRequest;
 import com.swampbits.misere.HttpResponse;
 
@@ -17,7 +21,40 @@ public class ServerStatsHandler extends AbstractHandler {
    @Override
    public void serviceRequest(HttpRequest request,
                               HttpResponse response) {
-      
+      StringBuilder body = new StringBuilder("<html><body>");
+      Logger logger = Logger.getLogger();
+
+      if (logger != null) {
+         Logger loggerInstance = logger;
+         if (loggerInstance instanceof StdLogger) {
+            StdLogger stdLogger = (StdLogger) loggerInstance;
+            Map<String, Long> mapLogLevelOccurrences
+                    = stdLogger.populateLogLevelOccurrences();
+
+            if ((null != mapLogLevelOccurrences) && !mapLogLevelOccurrences.isEmpty()) {
+               body.append("<table border=\"1\">");
+               body.append("<tr><th align=\"left\">Log Level</th><th>Occurrences</th></tr>");
+
+               for (String logLevel : mapLogLevelOccurrences.keySet()) {
+                  body.append("<tr><td>");
+                  body.append(logLevel);
+                  body.append("</td><td>");
+                  body.append(mapLogLevelOccurrences.get(logLevel));
+                  body.append("</td></tr>");
+               }
+
+               body.append("</table>");
+            } else {
+               body.append("No log level stats available");
+            }
+         } else {
+            body.append("No log level stats available (must use StdLogger)");
+         }
+
+         body.append("</body></html>");
+
+         response.setBody(body.toString());
+      }
    }
 
 }
