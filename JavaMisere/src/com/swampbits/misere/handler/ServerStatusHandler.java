30fe36c538766d628aef9fe69c1e52d5a5eedaf6
pauldardeau <pauldardeau@me.com>
2017-04-19 10:33:25 -0500

    implement server status handler

--- JavaMisere/src/com/swampbits/misere/handler/ServerStatusHandler.java
+++ JavaMisere/src/com/swampbits/misere/handler/ServerStatusHandler.java
@@ -5,6 +5,9 @@
  */
 package com.swampbits.misere.handler;
 
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+
 import com.swampbits.misere.HttpRequest;
 import com.swampbits.misere.HttpResponse;
 
@@ -17,7 +20,40 @@ public class ServerStatusHandler extends AbstractHandler {
    @Override
    public void serviceRequest(HttpRequest request,
                               HttpResponse response) {
+      OperatingSystemMXBean osMXBean =
+              ManagementFactory.getOperatingSystemMXBean();
+      StringBuilder body = new StringBuilder("<html><body>");
+      body.append("<table border=\"1\">");
+      body.append("<tr><th align=\"left\">Metric</th><th>Value</th></tr>");
       
+      body.append("<tr><td>Arch</td>");
+      body.append("<td>");
+      body.append(osMXBean.getArch());
+      body.append("</td></tr>");
+
+      body.append("<tr><td>Processors</td>");
+      body.append("<td>");
+      body.append(osMXBean.getAvailableProcessors());
+      body.append("</td></tr>");
+
+      body.append("<tr><td>OS Name</td>");
+      body.append("<td>");
+      body.append(osMXBean.getName());
+      body.append("</td></tr>");
+
+      body.append("<tr><td>OS Version</td>");
+      body.append("<td>");
+      body.append(osMXBean.getVersion());
+      body.append("</td></tr>");
+
+      body.append("<tr><td>System Load Average</td>");
+      body.append("<td>");
+      body.append(osMXBean.getSystemLoadAverage());
+      body.append("</td></tr>");
+
+      body.append("</table></body></html>");
+      
+      response.setBody(body.toString());
    }
 
 }
